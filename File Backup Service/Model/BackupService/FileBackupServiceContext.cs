8478f147ef2928ad19d7000fc376a0edebe75b16
John Dave B. Sevilla <jdbsevilla30@gmail.com>
2024-05-08 01:12:52 +0800

    [Jd] - Entity setup

---
+++ File Backup Service/Model/BackupService/FileBackupServiceContext.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace File_Backup_Service.Model.BackupService;
+
+public partial class FileBackupServiceContext : DbContext
+{
+    public FileBackupServiceContext()
+    {
+    }
+
+    public FileBackupServiceContext(DbContextOptions<FileBackupServiceContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<FileDirectory> FileDirectories { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Data Source=DESKTOP-IHC6K02\\MSSQLSERVER01;Initial Catalog=FileBackupService;Trusted_Connection=True;TrustServerCertificate=True;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<FileDirectory>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__FileDire__3214EC2719E514EF");
+
+            entity.ToTable("FileDirectory");
+
+            entity.Property(e => e.Id).HasColumnName("ID");
+            entity.Property(e => e.CreatedDate).HasColumnType("date");
+            entity.Property(e => e.ModifiedDate).HasColumnType("date");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ File Backup Service/Model/BackupService/FileDirectory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace File_Backup_Service.Model.BackupService;
+
+public partial class FileDirectory
+{
+    public int Id { get; set; }
+
+    public int? DirectoryId { get; set; }
+
+    public string? DirectoryName { get; set; }
+
+    public string? CreatedBy { get; set; }
+
+    public DateTime? CreatedDate { get; set; }
+
+    public string? ModifiedBy { get; set; }
+
+    public DateTime? ModifiedDate { get; set; }
+}
